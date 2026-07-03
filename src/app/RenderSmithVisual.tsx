27a2562f62bbe0a7f1de72b463bc37a1b62f40f4
Mason Perry <masoncalcolsol@gmail.com>
2026-07-03 15:24:33 -0700

    Add RenderSmith visual component

---
+++ src/app/RenderSmithVisual.tsx
@@ -0,0 +1,9 @@
+export default function RenderSmithVisual() {
+  return (
+    <img
+      src="/rendersmith-oi-suite-hero.svg"
+      alt="NULLWORKS OI SUITe RenderSmith visual master"
+      style={{ width: "100%", height: "100%", objectFit: "contain" }}
+    />
+  );
+}
