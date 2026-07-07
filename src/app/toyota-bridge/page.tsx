8e55883a0d2480e2077ce8f373217b9a85e29fb8
Mason Perry <masoncalcolsol@gmail.com>
2026-07-07 09:17:27 -0700

    publish detailed recruiter field case

---
+++ src/app/toyota-bridge/page.tsx
@@ -0,0 +1,9 @@
+import SoftwareProjectEngineerBridgePage, {
+  metadata,
+} from "../field-notes/software-project-engineer-bridge/page";
+
+export { metadata };
+
+export default function ToyotaBridgePage() {
+  return <SoftwareProjectEngineerBridgePage />;
+}
