ea76a6c1950f12518292284eeaf8ea0fba807ae6
Mason Perry <masoncalcolsol@gmail.com>
2026-07-12 09:18:19 -0700

    Load canonical NULLWORKS Mars hero styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./oisa.css";
+import "./mars-hero.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
