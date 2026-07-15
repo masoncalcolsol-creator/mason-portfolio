f2d7fa9d45ffa6e67750d6229cc7772ef520d1ab
Mason Perry <masoncalcolsol@gmail.com>
2026-07-15 10:47:04 -0700

    Apply Mr Sloth luxury visual system to Japanese page

---
+++ src/app/jp/mr-sloth/layout.tsx
@@ -0,0 +1,5 @@
+import '../../mr-sloth/mr-sloth-luxury.css';
+
+export default function MrSlothJapaneseLayout({ children }: { children: React.ReactNode }) {
+  return <div className="mrSlothLuxury">{children}</div>;
+}
