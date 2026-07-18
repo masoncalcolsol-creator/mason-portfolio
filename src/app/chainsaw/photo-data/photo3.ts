5fee9b45629f7ed5e2988dd0027b0427375b635d
Mason Perry <masoncalcolsol@gmail.com>
2026-07-18 05:01:53 -0700

    Assemble chainsaw AVIF photo 3

--- src/app/chainsaw/photo-data/photo3.ts
+++ src/app/chainsaw/photo-data/photo3.ts
@@ -1,1 +1,8 @@
-export const photo3 = "";
+import chunk0 from "./photo3-v6-chunks/chunk0";
+import chunk1 from "./photo3-v6-chunks/chunk1";
+import chunk2a from "./photo3-v6-chunks/chunk2a";
+import chunk2b from "./photo3-v6-chunks/chunk2b";
+import chunk3a from "./photo3-v6-chunks/chunk3a";
+import chunk3b from "./photo3-v6-chunks/chunk3b";
+
+export const photo3 = chunk0 + chunk1 + chunk2a + chunk2b + chunk3a + chunk3b;
