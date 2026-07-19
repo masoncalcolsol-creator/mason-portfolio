420c9555093719a2131218a8def4352a1b59602b
Mason Perry <masoncalcolsol@gmail.com>
2026-07-19 04:13:35 -0700

    Mount ANVIL ghost-scroll layer on portfolio pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AnvilGhostScroll from "./components/AnvilGhostScroll";
 import "./globals.css";
 import "./oisa.css";
 import "./mars-hero.css";
@@ -39,7 +40,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <AnvilGhostScroll />
+        {children}
+      </body>
     </html>
   );
 }
