11790cc185792186c72ca407a4ca89438139a3ee
Mason Perry <masoncalcolsol@gmail.com>
2026-08-02 10:18:11 -0700

    Apply 50 percent transparency tune to Paper II panels

--- src/app/operation-moonshot/observation-system/layout.tsx
+++ src/app/operation-moonshot/observation-system/layout.tsx
@@ -1,10 +1,11 @@
 import type { ReactNode } from "react";
 import BloodMoonSignal from "./BloodMoonSignal";
 import blood from "./bloodmoon.module.css";
+import tune from "./transparency-tune.module.css";
 
 export default function ObservationSystemLayout({ children }: { children: ReactNode }) {
   return (
-    <div className={blood.frame}>
+    <div className={`${blood.frame} ${tune.tune}`}>
       <BloodMoonSignal />
       <div className={blood.content}>{children}</div>
     </div>
