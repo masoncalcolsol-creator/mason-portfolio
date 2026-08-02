2929946ff1c8e77bdd5be6a9e512aee0453a7018
Mason Perry <masoncalcolsol@gmail.com>
2026-08-02 08:06:48 -0700

    Connect Paper II to Living Signals blood moon canvas

--- src/app/operation-moonshot/observation-system/layout.tsx
+++ src/app/operation-moonshot/observation-system/layout.tsx
@@ -1,6 +1,12 @@
 import type { ReactNode } from "react";
+import BloodMoonSignal from "./BloodMoonSignal";
 import blood from "./bloodmoon.module.css";
 
 export default function ObservationSystemLayout({ children }: { children: ReactNode }) {
-  return <div className={blood.frame}>{children}</div>;
+  return (
+    <div className={blood.frame}>
+      <BloodMoonSignal />
+      <div className={blood.content}>{children}</div>
+    </div>
+  );
 }
