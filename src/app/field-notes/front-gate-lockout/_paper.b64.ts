4771657bcfc382849922f5ed34337960861bfbc9
Mason Perry <masoncalcolsol@gmail.com>
2026-08-31 07:40:11 -0700

    Add Front Gate lockout paper and poster binary modules

---
+++ src/app/field-notes/front-gate-lockout/_paper.b64.ts
@@ -0,0 +1,4 @@
+import { PAPER_PDF_B64_A } from "./_paper.b64.a";
+import { PAPER_PDF_B64_B } from "./_paper.b64.b";
+const PAPER_PDF_B64 = PAPER_PDF_B64_A + PAPER_PDF_B64_B;
+export default PAPER_PDF_B64;
---
+++ src/app/field-notes/front-gate-lockout/_poster.b64.ts
@@ -0,0 +1,4 @@
+import { POSTER_JPEG_B64_A } from "./ _poster.b64.a";
+import { POSTER_JPEG_B64_B } from "./_poster.b64.b";
+const POSTER_JPEG_B64 = POSTER_JPEG_B64_A + POSTER_JPEG_B64_B;
+export default POSTER_JPEG_B64;
